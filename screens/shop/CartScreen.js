8987e4c0c29ecc1a275e7d684fe37519d8a91dd8
Gagana Gavesh <gaveshgamage@gmail.com>
2023-03-20 23:54:07 +0530

    CartItem logic added

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -5,7 +5,25 @@ import { useSelector } from "react-redux";
 //we can use nested Text tags so that we can give them different styles colors so on.
 
 const CartScreen = (props) => {
+  //
   const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
+  const cartItems = useSelector((state) => {
+    const transformedCartItems = [];
+    //map cant be used because items is not an array,it is an object
+    //so we have to get the keys and go through each objects
+    for (const key in state.cart.items) {
+      //since this is an array we can push any item for this
+      transformedCartItems.push({
+        productId: key,
+        productTitle: state.cart.items[key].productTitle,
+        productPrice: state.cart.items[key].productPrice,
+        quantity: state.cart.items[key].quantity,
+        sum: state.cart.items[key].sum,
+      });
+    }
+    return transformedCartItems;
+  });
+
   return (
     <View>
       <View>
